c908e6a994d2f45b7ac92731a3f43caecd10691d
liqilong <634505724@qq.com>
2017-11-09 10:37:50 +0800

    url and request

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-webpy
 python中的webpy框架搭建的小项目
+
+- webpy框架只支持python2
+- 下载源码后https://github.com/webpy/webpy/tarball/master 在命令行中运行 python2 setup.py install 安装成功
+
+
---
+++ request.py
@@ -0,0 +1,28 @@
+import web
+
+urls = (
+	'/index', 'index',
+	'/blog/\d+', 'blog',
+	'/(.*)', 'hello',
+)
+app = web.application(urls, globals())
+
+class index:
+	def GET(self):
+		query = web.input()
+		return query
+
+class blog:
+	def POST(self):
+		data = web.input()
+		return data
+
+	def GET(self):
+		return web.ctx.env
+		
+class hello:
+	def GET(self, name):
+		return open(r'request.html').read()
+
+if __name__=="__main__":
+	app.run()
